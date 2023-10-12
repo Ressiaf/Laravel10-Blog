4722ead59985ba58e40a4481c339c108cabf0b6a
ressiaf <facurd91@gmail.com>
2023-10-12 20:13:28 -0300

    blog-rutes

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return "Root";
+});
+
+
+Route::get('courses' , function (){
+    return "Courses route";
+});
+
+Route::get('courses/{course}' , function ($course){
+    return "Welcome to : $course";
 });
